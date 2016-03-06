16e9053097d96257f0dd90c3e724a989a9b26777
Thibault <tallancon@gmail.com>
2016-03-06 19:50:15 +0100

    Added portability

--- src/prompt.c
+++ src/prompt.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Windows */
+#ifdef _WIN32
+#include <string.h>
+
+#define BUFFER_SIZE 2048
+
+static char buffer[BUFFER_SIZE];
+
+char *readline(char *prompt)
+{
+   fputs(prompt, stdout);
+   fgets(buffer, BUFFER_SIZE, stdin);
+
+   char *copy;
+   copy = malloc(strlen(buffer) + 1);
+   strcpy(copy, buffer);
+   copy[strlen(copy) - 1] = '\0';
+
+   return copy;
+}
+
+void add_history(char *unused)
+{
+
+}
+
+/* Linux and Mac */
+#else
 #include <editline/readline.h>
 #include <histedit.h>
+#endif
 
 int main(void)
 {
